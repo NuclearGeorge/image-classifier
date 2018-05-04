dd0ba4ec2c88dc06545c0e3b0c63deefb14c9ea9
NuclearGeorge <38961339+NuclearGeorge@users.noreply.github.com>
2018-05-04 13:43:33 -0500

    Create IC_JCHC.m

---
+++ IC_JCHC.m
@@ -0,0 +1,4 @@
+Off[CreateDirectory::ioerr];
+Print[StyleForm["=====================================================","Section",FontSize->14]]
+Print[StyleForm["PACKAGE:","Section",FontSize->14],StyleForm[" IMAGE CLASSIFIER","Section",FontSize->14] ]
+Print[StyleForm["BY: Jorge Chávez Carlos, 2018","Section",FontSize->12]]
